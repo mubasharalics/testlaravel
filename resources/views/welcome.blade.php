281e68e94a516e4ce1aeeb67e7865614a9deefea
Mubashar Ali <mubasharmscs@gmail.com>
2017-07-23 21:03:17 -0700

    adding js and css libraries

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Skill Test</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
 
         <!-- Styles -->
         <style>
@@ -67,14 +68,14 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
+            <div class="top-right links">
+                @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+                @endif
+            </div>
             @endif
 
             <div class="content">
@@ -91,5 +92,7 @@
                 </div>
             </div>
         </div>
+        <script src="{{ asset('js/jquery-3.2.1.min.js') }}"  type="text/javascript" ></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
     </body>
 </html>
