e97e2cfe9bac1e4e7075497519807db4d90e8a3d
Mubashar Ali <mubasharmscs@gmail.com>
2017-07-23 21:45:40 -0700

    migration for products tabele is created and migrated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,61 +9,10 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
-
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+        
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -91,6 +40,10 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+        </div>
+        <div class="text-center">
+            <h1>Product Form</h1>
+            
         </div>
         <script src="{{ asset('js/jquery-3.2.1.min.js') }}"  type="text/javascript" ></script>
         <script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
